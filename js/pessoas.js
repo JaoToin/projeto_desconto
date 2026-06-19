14dfa39cdb3919622397a04001f3245c25032fb9
joao <joao.a.guedes@aluno.senai.br>
2026-06-19 18:38:20 -0300

    html logar com js

---
+++ js/pessoas.js
@@ -0,0 +1,11 @@
+//CRIAR UM ARRAY DE OBJETO PESSOA 
+
+const pessoa = []
+
+// PEGANDO ELEMENTOS DO DOM
+
+const formPessoa = document.querySelector('#div-form')
+const divPessoa = document.querySelector('#div-lista-pessoa')
+
+// CAPTURANDO O EVENTO DO SUBMIT DO FORMULARIO
+
